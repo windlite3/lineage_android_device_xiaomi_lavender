4443e4c1f382276f17cf855c213030ee32851b19
windlite <128964303+windlite3@users.noreply.github.com>
2025-08-17 01:29:42 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -17,8 +17,8 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     libstdc++.vendor \
-    libMegviiFacepp-0.5.2 \
     libmegface
+#    libMegviiFacepp-0.5.2 \
 
 # Consumerir
 BOARD_HAVE_IR := true
